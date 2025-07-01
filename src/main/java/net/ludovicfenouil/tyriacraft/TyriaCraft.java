e1960ed145c32b6ed23e6333ff3c1f6a7e79b488
ludovicfenouil <fenouil.ludovic@sfr.fr>
2025-07-01 23:45:16 +0200

    Recreated all the leaves blocks as in the forge version

--- src/main/java/net/ludovicfenouil/tyriacraft/TyriaCraft.java
+++ src/main/java/net/ludovicfenouil/tyriacraft/TyriaCraft.java
@@ -2,6 +2,9 @@ package net.ludovicfenouil.tyriacraft;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.ludovicfenouil.tyriacraft.block.TC_Blocks;
+import net.ludovicfenouil.tyriacraft.item.TC_ItemGroups;
+import net.ludovicfenouil.tyriacraft.item.TC_Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +14,8 @@ public class TyriaCraft implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		TC_ItemGroups.registerItemGroups();
+		TC_Items.registerTC_Items();
+		TC_Blocks.registerTC_Blocks();
 	}
 }
